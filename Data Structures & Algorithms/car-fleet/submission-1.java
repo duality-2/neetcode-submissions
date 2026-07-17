3cdc954aaf721040d63627603ec2db10caf6adaa
Yash Bhurke <yashbhurke69@gmail.com>
2026-07-17 14:01:57 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] cars = new int[n][2];
+        Stack<Double> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+        Arrays.sort(cars, (a, b) -> b[0] - a[0]); // sorting in desc
+        for (int[] car : cars) {
+            double time = (double) (target - car[0]) / car[1];
+            if (!stack.isEmpty() && time <= stack.peek())
+                continue;
+            else
+                stack.push(time);
+        }
+        return stack.size();
+    }
+}
